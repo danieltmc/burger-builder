e4385389fdab9c32ffd32bf94a89ef781d9b03f7
danieltmc <me@danieltmc.com>
2020-03-06 09:47:36 -0500

    Create project Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
+
+import Layout from './components/Layout/Layout';
 
 function App() {
-  render() {
-    return (
-      <div>
-
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
