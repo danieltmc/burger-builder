b9972318100e8d9fdf6b79add41e4cedfe028f75
danieltmc <me@danieltmc.com>
2020-03-06 12:37:15 -0500

    Dynamically remove ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,6 +7,7 @@ const buildControl = (props) => (
     <div className={classes.Label}>{props.label}</div>
     <button
       className={classes.Less}
+      onClick={props.removed}
     >
       Less
     </button>
