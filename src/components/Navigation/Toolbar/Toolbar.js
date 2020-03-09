19bccf3b124374ea51436ed0b4af5d6d113e3f56
danieltmc <me@danieltmc.com>
2020-03-09 09:52:10 -0400

    Make Toolbar and Navigation CSS responsive

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,10 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
   <header classNamme={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>
+    <div className={classes.Logo}>
+      <Logo height="80%" />
+    </div>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
